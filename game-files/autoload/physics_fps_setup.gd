82d09744bed8939d159f9e461dfd23406a0bfe34
FelipeBdC <batatanotube@gmail.com>
2024-02-29 12:06:51 -0300

    added proper loading of the main scene

--- game-files/autoload/physics_fps_setup.gd
+++ game-files/autoload/physics_fps_setup.gd
@@ -5,4 +5,4 @@ func _ready():
 	var refresh_rate: float = DisplayServer.screen_get_refresh_rate()
 	if refresh_rate <= 0.0:
 		refresh_rate = 60.0
-	Engine.physics_ticks_per_second = refresh_rate
+	Engine.physics_ticks_per_second = int(refresh_rate)
